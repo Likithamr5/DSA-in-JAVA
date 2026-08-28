284bfe9492edeec3364454fbaac21a7d8f8dee20
Likithamr5 <likithamr82@gmail.com>
2026-08-28 22:12:18 +0530

    Implement Bubble Sort algorithm in BubbleSort.java

---
+++ GreeksForGreeks/BubbleSort.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void bubbleSort(int[] arr) {
+        // code here
+        int n = arr.length;
+        
+        for(int i=n-1;i>=1;i--){
+            int swap = 0;
+            for(int j=0;j<=i-1;j++){
+                 if(arr[j] > arr[j + 1]) {
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+                
+                swap = 1;
+            }
+            }
+        if (swap == 0){
+            break;
+           }
+        }
+    }
+}
