e4cfb5599450e00b882dfacd38219a19d9f0efd5
Likithamr5 <likithamr82@gmail.com>
2026-09-03 23:27:31 +0530

    Add isSorted method to check sorted array
    Implement method to check if an array is sorted.

---
+++ GreeksForGreeks/Check_sorted_array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isSorted(int[] arr) {
+        // code here
+        //int arr.length = n;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i] >= arr[i-1]){
+            
+        }else{
+            return false;
+        }
+    }
+    return true;
+    }
+}
